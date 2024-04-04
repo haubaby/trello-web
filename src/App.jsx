5f131c9aed497a75e9408a9076b057a9709d5008
haubaby <104343322+haubaby@users.noreply.github.com>
2024-04-04 15:42:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button'
+
+
+function App() {
+  return (
+    <>
+      <div>hellohau</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  )
+}
+
+export default App
